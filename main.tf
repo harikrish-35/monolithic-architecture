5a278244a56ba332bf3eb22d9c195262223de9a4
root <root@ip-172-31-86-16.ec2.internal>
2024-03-30 09:38:41 +0000

    files uploaded

---
+++ main.tf
@@ -0,0 +1,34 @@
+resource "aws_launch_configuration" "web_server_as" {
+    image_id           = "ami-0830c9faf0efc29ff"
+    instance_type = "t2.micro"
+    key_name = "manish"
+}
+   
+
+
+  resource "aws_elb" "web_server_lb"{
+     name = "web-server-lb"
+     security_groups = [aws_security_group.web_server.id]
+     subnets = ["subnet-09f13356f04fffb8b", "subnet-0f8c7425356725a9f"]
+     listener {
+      instance_port     = 8000
+      instance_protocol = "http"
+      lb_port           = 80
+      lb_protocol       = "http"
+    }
+    tags = {
+      Name = "terraform-elb"
+    }
+  }
+resource "aws_autoscaling_group" "web_server_asg" {
+    name                 = "web-server-asg"
+    launch_configuration = aws_launch_configuration.web_server_as.name
+    min_size             = 1
+    max_size             = 3
+    desired_capacity     = 2
+    health_check_type    = "EC2"
+    load_balancers       = [aws_elb.web_server_lb.name]
+    availability_zones    = ["us-west-1a", "us-west-1b"] 
+    
+  }
+
